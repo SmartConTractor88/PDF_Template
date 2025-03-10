8c4ab930a11d340a48ecca6f912b3b832e7523e4
SmartConTractor <login.blockchain888@gmail.com>
2025-03-10 22:34:10 +0100

    Create a nested for-loop to add multiple pages for each topic. Remove borders from headings.

--- main.py
+++ main.py
@@ -9,22 +9,18 @@ df = pd.read_csv("topics.csv")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4") # P for portrait, L for landscape
 
-pdf.add_page() # add a page
 
-pdf.set_font(family="Times", style="B", size=24)
-
-pdf.cell(w=0, h=24, txt="PDF Template", align="C", ln=1, border=1)
-pdf.cell(w=0, h=24, txt="", align="C", ln=1, border=0) # empty cell
-
-pdf.set_font(family="Times", size=18)
-
-pdf.cell(w=0, h=24, txt="PDF Template", align="C", ln=1, border=1)
 
 for index, row in df.iterrows():
+    
     pdf.add_page() # add a page
+
     print(row["Topic"])
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.cell(w=0, h=24, txt=row["Topic"], align="C", ln=1, border=1)
-    pdf.line(20, 27, 190, 27)
+    pdf.cell(w=0, h=24, txt=row["Topic"], align="C", ln=1, border=0)
+    pdf.line(30, 28, 180, 28)
+    
+    for page in range(row["Pages"] - 1):
+        pdf.add_page() # add a page
     
 pdf.output("first_pdf.pdf")

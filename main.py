3be816659b974909ea16ee95e9254292011261cc
SmartConTractor <login.blockchain888@gmail.com>
2025-03-10 22:50:53 +0100

    Add footers in each page.

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ df = pd.read_csv("topics.csv")
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4") # P for portrait, L for landscape
-
-
+pdf.set_auto_page_break(auto=False, margin=0) # pages should not be broken automatically
 
 for index, row in df.iterrows():
     
@@ -17,10 +16,24 @@ for index, row in df.iterrows():
 
     print(row["Topic"])
     pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(0, 0, 0)
     pdf.cell(w=0, h=24, txt=row["Topic"], align="C", ln=1, border=0)
     pdf.line(30, 28, 180, 28)
+
+    pdf.ln(250) # 250 mm of break lines
+
+    pdf.set_font(family="Times", style="I", size=12)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="C", ln=1, border=0)
+
     
     for page in range(row["Pages"] - 1):
         pdf.add_page() # add a page
+
+        pdf.ln(274) # 250 mm of break lines
+
+        pdf.set_font(family="Times", style="I", size=12)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="C", ln=1, border=0)
     
 pdf.output("first_pdf.pdf")
